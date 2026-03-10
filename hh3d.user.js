e88ee8f46208a4ee2a5a4f310816d027e19ff986
vuongmatu <lengocthuc1656@gmail.com>
2026-03-10 23:32:26 +0700

    Update hh3d.user.js

--- hh3d.user.js
+++ hh3d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HH3D
 // @namespace    https://github.com/hoathinh3d173820-coder
-// @version      5.4
+// @version      5.5
 // @description  Script HH3D
 // @match        *://*/*
 // @grant        GM_addStyle
