31f8b493d337246de69aba22717224eb8966ee89
vuongmatu <lengocthuc1656@gmail.com>
2026-03-25 23:04:04 +0700

    Update hh3d.user.js

--- hh3d.user.js
+++ hh3d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HH3D
 // @namespace    https://github.com/hoathinh3d173820-coder
-// @version      1.5
+// @version      1.6
 // @description  Script HH3D
 // @match        *://*/*
 // @grant        GM_addStyle
